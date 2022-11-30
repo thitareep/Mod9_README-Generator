e0f4024379a0cdee8c48766de892a61541ed5b24
Thitareep <thitareep@hotmail.com>
2022-11-30 00:48:56 -0600

    added function to generate README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,6 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+// MIT", "APACHE", "Eclipse PL", "GNU GPL v3.0"
 function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
@@ -12,8 +13,45 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+  ${renderLicenseBadge(data.license)}}
+
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Tests](#test)
+  * [Contribution](#contribution)
+  * [Questions](#questions)
+  * [Credits](#credits)
+
+  ## Description
+  ${data.description}
+  
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+  
+  ## License
+  ${data.license}
+  
+  ## Tests
+  ${data.test}
+
+  ## Questions
+  Have any questions about this project/application?
+  Contact:
+  GitHub | https://github.com/${data.userName}
+  Email | ${data.email}
+
+  ## Credits
+  Created by:
+  ${data.credits}
 `;
 }
 
